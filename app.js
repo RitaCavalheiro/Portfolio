d7413f67dfdd3dd2346198d3fea2b1332734e46b
RitaCavalheiro <44837728+RitaCavalheiro@users.noreply.github.com>
2021-12-28 19:12:19 +0000

    added hamburger nav to mobile view

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById("nav-ul");
 
+hamburger.addEventListener("click", () => {
+  navUL.classList.toggle("show");
+});
