8d936aad30b1950dd7328fb2fd6666ba114983ae
RitaCavalheiro <44837728+RitaCavalheiro@users.noreply.github.com>
2024-03-23 19:20:13 +0000

    Updated Portfolio

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const navUL = document.getElementById("nav-ul");
 hamburger.addEventListener("click", () => {
   navUL.classList.toggle("show");
 });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
